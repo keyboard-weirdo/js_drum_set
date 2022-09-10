e22f736e3c5b6f3722e80649bac3bf0d4cb7bad3
keyboard-weirdo <connoisseuse.of.weird.things@gmail.com>
2022-09-10 15:09:36 +0300

    delete js.js file add javascript to html body finish all coding needed still need to change audio files

--- js.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    console.log(audio);
-    if(!audio) return; //stops the function from running
-    audio.play();
-});
